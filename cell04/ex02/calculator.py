f71e1351211a0b9e78379e9b49d7b3cd08034b4e
jeena5555 <jeenapeamm@gmail.com>
2025-09-18 20:00:24 +0700

    Finish cell04

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,5 +1,13 @@
-x = [2, 8, 9, 48, 8, 22, -12, 2]
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+print("Thank you!")
 
-result = list(dict.fromkeys([n + 2 for n in x if n > 5]))
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
 
-print(result)
+if num1 % num2 == 0:
+    print(f"{num1} / {num2} = {num1 // num2}")
+else:
+    print(f"{num1} / {num2} = {num1 / num2}")
+
+print(f"{num1} * {num2} = {num1 * num2}")
